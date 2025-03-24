c3964388b032adf87d4e4f7af1e896f20d52ce2c
MilgramHTL <lukas.wagner@student.htldornbirn.at>
2025-03-24 16:36:54 +0100

    Added MockAPI Data

--- src/components/PeopleContainer.jsx
+++ src/components/PeopleContainer.jsx
@@ -1,25 +1,29 @@
 import React from "react";
 import Card from "./CardE03";
+import { useState } from "react";
+import { useEffect } from "react";
+
 export default function PeopleContainer() {
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    fetch("https://67b845fc699a8a7baef35fe5.mockapi.io/person")
+      .then((res) => res.json())
+      .then((data) => setPeople(data));
+  }, []);
   return (
     <div>
       <h1>People</h1>
-      <div className="w-60">
-        <Card
-          name="John Doe"
-          jobtitle="Software Engineer"
-          picture="https://picsum.photos/200/300"
-        />
-        <Card
-          name="Jane Doe"
-          jobtitle="Software Engineer"
-          picture="https://picsum.photos/200/300"
-        />
-        <Card
-          name="John Smith"
-          jobtitle="Software Engineer"
-          picture="https://picsum.photos/200/300"
-        />
+      <div>
+        {people.map((element) => {
+          return <div className="w-40">Card({
+            name: element.name,
+            jobtitle: element.jobtitle,
+            picture: element.avatar,
+          });
+          </div>
+        })}
+        ;
       </div>
     </div>
   );

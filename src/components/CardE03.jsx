6edef635f9fbbad9f1b95a2b369e7c9b0a9d6c8f
MilgramHTL <lukas.wagner@student.htldornbirn.at>
2025-03-25 14:21:51 +0100

    Finished 05 Demo Filter

--- src/components/CardE03.jsx
+++ src/components/CardE03.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 export default function Card({ name, jobtitle, picture }) {
   return (
-    <div className="border grid grid-cols-2 h-40">
+    <div className="border grid grid-cols-2 h-50">
       <div className="p-4">
         <h2 className="font-bold text-xl">{name}</h2>
         <p>{jobtitle}</p>
       </div>
       <div className="w-full">
-        <img src={picture} alt="" className="object-cover w-full h-40"></img>
+        <img src={picture} alt="" className="object-cover w-full h-full"></img>
       </div>
     </div>
   );
--- src/components/PeopleContainer.jsx
+++ src/components/PeopleContainer.jsx
@@ -5,16 +5,38 @@ import { useEffect } from "react";
 
 export default function PeopleContainer() {
   const [people, setPeople] = useState([]);
+  const [filteredPeople, setFilteredPeople] = useState([]);
 
   useEffect(() => {
     fetch("https://67b845fc699a8a7baef35fe5.mockapi.io/person")
       .then((res) => res.json())
-      .then((data) => setPeople(data));
+      .then((data) => {
+        setPeople(data);
+        setFilteredPeople(data);
+      });
   }, []);
+
+  const filterPeople = (filter) => {
+    let filtered = people.filter((person) =>
+      person.name.toLowerCase().includes(filter.toLowerCase())
+    );
+    setFilteredPeople(filtered);
+  };
   return (
-    <div>
-      <div className="grid grid-cols-2 lg:grid-cols-4 gap-4">
-        {people.map((element) => {
+    <div className="p-8">
+      <div className="fixed border mb-2 bg-amber-100 w-10vw h-28 p-4">
+        <input
+          className="border p-3"
+          placeholder="Search"
+          type="text"
+          onChange={(el) => {
+            console.log(el.target.value);
+            filterPeople(el.target.value);
+          }}
+        />
+      </div>
+      <div className="bg-blue-200 grid grid-cols-2 lg:grid-cols-4 gap-2 p-6 pt-32">
+        {filteredPeople.map((element) => {
           return (
             <div>
               <Card
@@ -25,7 +47,6 @@ export default function PeopleContainer() {
             </div>
           );
         })}
-        ;
       </div>
     </div>
   );
